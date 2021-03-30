03aed6f0898da314b25a8f93e7b03cd44a27864b
= <=>
2021-03-31 00:27:22 +0530

    feature:listing movies,Added moviesDto

--- movie-ticket-cinema/src/main/java/com/movieticket/service/MovieServiceImpl.java
+++ movie-ticket-cinema/src/main/java/com/movieticket/service/MovieServiceImpl.java
@@ -1,5 +1,6 @@
 package com.movieticket.service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,15 +10,32 @@ import com.movieticket.dao.MovieRepositoryLocal;
 import com.movieticket.dto.MoviesDto;
 import com.movieticket.entity.Cinema;
 
+/**
+ * 
+ * @author Priya.Prasad
+ *
+ */
 @Service
 public class MovieServiceImpl implements MovieServiceLocal {
-	
+
 	@Autowired
 	MovieRepositoryLocal movieRepository;
-
+	
 	@Override
-	public List<Cinema> getAllMovies() {
-		return movieRepository.findAll();
+	public List<MoviesDto> getAllMovies() {
+		List<MoviesDto> listMovies = new ArrayList<>();
+		MoviesDto moviesDto = new MoviesDto();
+		List<Cinema> cinemas = movieRepository.findAll();
+		for (Cinema cinema : cinemas) {
+			moviesDto.setCinemaId(cinema.getCinemaId());
+			moviesDto.setGenre(cinema.getGenre());
+			moviesDto.setEndTime(cinema.getEndTime());
+			moviesDto.setStartTime(cinema.getStartTime());
+			moviesDto.setMovieDirector(cinema.getMovieDirector());
+			moviesDto.setMovieName(cinema.getMovieName());
+			listMovies.add(moviesDto);
+		}
+		return listMovies;
 	}
 
 }
